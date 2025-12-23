341566606d1bacb82a195ebe9d118413cafa2189
ImRady <imr4dy@gmail.com>
2025-12-23 16:25:20 +0700

    create module crad

--- card/src/main/java/com/bank/card/domain/entity/Card.java
+++ card/src/main/java/com/bank/card/domain/entity/Card.java
@@ -1,4 +1,25 @@
 package com.bank.card.domain.entity;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.annotation.Collation;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Document(collection = "cards")
+@Data
 public class Card {
+
+    @Id
+    private String cardId;
+    private String customerId;
+    private String cardNumber;
+    private String cardType;
+    private BigDecimal totalLimit;
+    private BigDecimal amountUsed;
+    private BigDecimal availableAmount;
+    private LocalDate createdDate;
 }
